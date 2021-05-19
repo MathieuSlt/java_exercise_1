dab985c67645c484d16128bad574cd94670bd782
Mathieu SALLIOT <mathieu.salliot@epita.fr>
2021-05-19 11:03:39 +0200

    Partie 5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,11 +1,7 @@
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Launcher {
     public static void main(String[] args) {
@@ -31,22 +27,43 @@ public class Launcher {
                 Path p = Paths.get(s);
                 try {
                     String content = java.nio.file.Files.readString(p);
-                    String[] lines = content.toLowerCase().split("\\r?\\n");
-                    for (String line : lines) {
-                        String[] newline = line.replaceAll("[()',.;:?!-]+"," ").split("\\s+");
-                        System.out.println();
-                        System.out.println(Arrays.toString(newline));
-                    }
+                    String[] lines = content.toLowerCase().replaceAll("[()',.;:?!-]+"," ").split("\\s+");
+                    freq(lines, 3);
                 } catch (IOException e) {
                     System.out.println("Unreadable file: " + e.getMessage());
                 }
-
             }
             else {
                 System.out.println("Unknown command");
             }
         }
     }
+    @SuppressWarnings("unchecked")
+    public static void freq(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words)
+            map.put(word, map.getOrDefault(word, 0) + 1);
+
+        int n = words.length;
+        List<String>[] buckets = new List[n + 1];
+
+        for (int i = 0; i < n + 1; i++)
+            buckets[i] = new ArrayList<>();
+        for (String key : map.keySet())
+            buckets[map.get(key)].add(key);
+
+        for (List<String> bucket : buckets)
+            Collections.sort(bucket);
+
+        List<String> top = new ArrayList<>();
+
+        for (int i = n; (i >= 0) && (top.size() < k); i--)
+            for (int j = 0; (j < buckets[i].size()) && (top.size() < k); j++)
+                top.add(buckets[i].get(j));
+
+        System.out.println(top.get(0) + " " + top.get(1) + " " + top.get(2));
+    }
+
     public static int fib(int n) {
         if (n <= 1) return n;
         else return fib(n-1) + fib(n-2);
